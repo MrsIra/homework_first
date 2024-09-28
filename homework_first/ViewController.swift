defe060f9d6f5d362d2f39c06f028dfca9463df0
Irina Bobova <irinabobova2005@gmail.com>
2024-09-28 23:46:58 +0700

    Initial Commit

---
+++ homework_first/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  homework_first
+//
+//  Created by Ира on 25.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
